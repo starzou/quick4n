08d5fd086e885f6a8a0880d9049f8e95be9d7427
StarZou <zoutongyuan@163.com>
2015-08-14 11:56:32 +0800

    dev

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,14 +1,30 @@
 /**
-* Users.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Users.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
-  attributes: {
+    attributes: {
 
-  }
+        id: {
+            type      : 'integer',
+            primaryKey: true,
+            unique    : true
+        },
+
+        name    : {
+            type: 'string'
+        },
+        password: {
+            type: 'string'
+        },
+        email   : {
+            type: 'string'
+        }
+
+    }
 };
 
